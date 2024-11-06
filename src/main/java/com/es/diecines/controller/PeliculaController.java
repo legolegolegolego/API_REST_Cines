66e7e8cc1f10c53df9f2a9475ca3e93cf0cc31cc
Lainezz <diegolinaresmojacar@gmail.com>
2024-11-06 17:10:56 +0100

    adicion  pasos post

--- src/main/java/com/es/diecines/controller/PeliculaController.java
+++ src/main/java/com/es/diecines/controller/PeliculaController.java
@@ -3,10 +3,7 @@ package com.es.diecines.controller;
 import com.es.diecines.dto.PeliculaDTO;
 import com.es.diecines.service.PeliculaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,9 +16,28 @@ public class PeliculaController {
 
     /**
      * Crear una pelicula
+     * El método para insertar es un POST -> @PostMapping
+     * El recurso para este método es localhost:8080/peliculas/
+     * /peliculas lo tenemos en el @RequestMapping arriba
+     * / en el método sólo ponemos la barra
+     *
+     * La información de la nueva Pelicula viene en el cuerpo de la petición
+     * Para obtener el cuerpo de la petición -> @RequestBody
      * @return
      */
-    public PeliculaDTO insert() {
+    @PostMapping("/")
+    public PeliculaDTO insert(
+            @RequestBody PeliculaDTO pDto
+    ) {
+
+        // 1º Comprobar que pDto no es null
+
+        // 2º LLamar al service, que es donde se hace la logica de negocio
+        // y por donde se accede a la BDD
+
+        // 3º Comprobar que lo que devuelve el service es null o no
+        // y responder
+
         return null;
     }
 
